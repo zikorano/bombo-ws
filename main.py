b7ecad42fd8621c7377596abc0e0bd50f030a950
Zikora Ogbuagu <robbie89339@gmail.com>
2022-07-14 16:54:32 +0100

    refactor: Setup for read operations in API

--- main.py
+++ main.py
@@ -4,27 +4,22 @@ from methods import ops
 
 app = Flask(__name__)
 
-@app.route("/api/players/<operation>", methods=["POST"])
-def player_api(operation: str):
-    request_args = request.get_json()
+@app.route("/api/players/<argument>", methods=["POST", "GET"])
+def player_api(argument: str):
+    if request.method == "POST":
+        operation = argument
+        request_args = request.get_json()
 
-    if (operation == "signup"):
-        return ops.sign_up(request_args)
+        if   (operation == "signup"):  return ops.sign_up(request_args)
+        elif (operation == "signin"):  return ops.sign_in(request_args)
+        elif (operation == "signout"): return ops.sign_out(request_args)
+        elif (operation == "update"):  return ops.update(request_args)
+        elif (operation == "delete"):  return ops.delete(request_args)
 
-    elif (operation == "signin"):
-        return ops.sign_in(request_args)
+        else: return api_error("Invalid operation")
 
-    elif (operation == "signout"):
-        return ops.sign_out(request_args)
-
-    elif (operation == "update"):
-        return ops.update(request_args)
-
-    elif (operation == "delete"):
-        return ops.delete(request_args)
-
-    else:
-        return api_error("Invalid operation")
+    elif request.method == "GET":
+        pass
 
 if __name__ == "__main__":
     app.run(debug=True)
